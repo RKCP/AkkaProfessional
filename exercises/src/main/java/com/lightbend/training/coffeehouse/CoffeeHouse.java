7883773d70dc9e402e761e99681f68f027d37877
PETERS Raphael <raphael.peters@idemia.com>
2025-04-10 11:37:55 +0100

    Add code to create actor

--- exercises/src/main/java/com/lightbend/training/coffeehouse/CoffeeHouse.java
+++ exercises/src/main/java/com/lightbend/training/coffeehouse/CoffeeHouse.java
@@ -1,11 +1,23 @@
 package com.lightbend.training.coffeehouse;
 
+import akka.actor.AbstractLoggingActor;
+import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+
 public class CoffeeHouse extends AbstractLoggingActor {
+
+    public CoffeeHouse() {
+        log().debug("CoffeeHouse Open");
+    }
 
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .matchAny(msg -> log().info("Coffee Elemental Brewing"))
                 .build();
+    }
+
+    public static Props props() {
+        return Props.create(CoffeeHouse.class, CoffeeHouse::new);
     }
 }
