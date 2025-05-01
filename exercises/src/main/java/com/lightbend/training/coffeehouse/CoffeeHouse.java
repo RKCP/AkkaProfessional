7fad26721d5d4e4ffba61c6a08c8c8a6617ca699
Raphael Peters <rkcpeters@gmail.com>
2025-05-01 15:54:42 +0100

    Further refactor

--- exercises/src/main/java/com/lightbend/training/coffeehouse/CoffeeHouse.java
+++ exercises/src/main/java/com/lightbend/training/coffeehouse/CoffeeHouse.java
@@ -1,10 +1,17 @@
 package com.lightbend.training.coffeehouse;
 
 import akka.actor.AbstractLoggingActor;
+import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 public class CoffeeHouse extends AbstractLoggingActor {
+
+    private final ActorRef waiter = createWaiter();
 
     public CoffeeHouse() {
         log().debug("CoffeeHouse Open");
@@ -13,22 +20,50 @@ public class CoffeeHouse extends AbstractLoggingActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
-                .match(CreateGuest.class, createGuest -> createGuest())
+                .match(CreateGuest.class, createGuest -> createGuest(createGuest.favouriteCoffee))
                 .build();
     }
 
-    protected void createGuest() {
-        context().actorOf(Guest.props()); // creates a child actor instead of a top level actor. (due to using context())
+    protected void createGuest(Coffee favouriteCoffee) {
+        context().actorOf(Guest.props(waiter, favouriteCoffee)); // creates a child actor instead of a top level actor. (due to using context())
     }
 
     public static Props props() {
         return Props.create(CoffeeHouse.class, CoffeeHouse::new);
     }
 
+    protected ActorRef createWaiter() {
+        return getContext().actorOf(Waiter.props(), "waiter");
+    }
+
     public static final class CreateGuest {
 
-        public static final CreateGuest Instance = new CreateGuest(); // cant create instances since constructor is private. Can only CreateGuest.Instance.
+//        public static final CreateGuest Instance = new CreateGuest(); // cant create instances since constructor is private. Can only CreateGuest.Instance.
+        public final Coffee favouriteCoffee;
 
-        private CreateGuest() {} // private constructor to not ruin the singleton pattern
+        public CreateGuest(Coffee favouriteCoffee) {
+            checkNotNull(favouriteCoffee, "Favourite coffee cannot be null");
+            this.favouriteCoffee = favouriteCoffee;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CreateGuest that = (CreateGuest) o;
+            return Objects.equals(favouriteCoffee, that.favouriteCoffee);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hashCode(favouriteCoffee);
+        }
+
+        @Override
+        public String toString() {
+            return "CreateGuest{" +
+                    "favouriteCofee=" + favouriteCoffee +
+                    '}';
+        }
     }
 }
